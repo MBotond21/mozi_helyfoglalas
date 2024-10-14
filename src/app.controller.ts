734c92a75c385ac4892d511cf53e7ae58b949ca4
MBotond21 <m.botond07@gmail.com>
2024-10-14 13:16:46 +0200

    majdnem kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Post, Res, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { DataFormDto } from './dataFormDto.sto';
 
 @Controller()
 export class AppController {
@@ -12,4 +14,51 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+  @Get('dataForm')
+  @Render('dataForm')
+  getDataForm(){
+    return {
+      data: {},
+      errors: []
+    }
+  }
+
+  @Post('dataForm')
+  postData(@Body() dataFormDto: DataFormDto, @Res() response: Response){
+    let errors = []
+
+    if(!dataFormDto.name || !dataFormDto.email || !dataFormDto.date || !dataFormDto.viewers){
+      errors.push("Minden mező kitöltése kötelező!");
+    }
+    if(!/[A-z]@[A-z]./.test(dataFormDto.email)){
+      errors.push("Email helyes fomrája: pelda@email.com");
+    }
+    if(Date.now() > new Date(dataFormDto.date).getTime()){
+      errors.push("A jelenlegi dátum előtti kiválasztása nem lehetséges")
+    }
+    if(parseInt(dataFormDto.viewers) < 1){
+      errors.push("Nem lehet 1-nél kevesebb néző számára helyet foglalni")
+    }
+    if(parseInt(dataFormDto.viewers) > 10){
+      errors.push("Nem lehet 10-nél több néző számára helyet foglalni")
+    }
+    
+    if(errors.length > 0){
+      response.render('dataForm', {
+        data: dataFormDto,
+        errors
+      });
+      return;
+    }
+
+    response.redirect('/success')
+
+  }
+
+  @Get('success')
+  getSuccess(){
+    return "Sikeres foglalás"
+  }
+
 }
---
+++ src/dataFormDto.sto.ts
@@ -0,0 +1,6 @@
+export interface DataFormDto {
+    name: string,
+    email: string,
+    date: string,
+    viewers: string
+}
